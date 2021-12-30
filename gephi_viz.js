c904f14d03a8712d399c61e39806f76dac69a182
Arjun Nair <arjunnair.mec@gmail.com>
2021-12-31 01:25:49 +0530

    Add zoom and drag control

--- gephi_viz.js
+++ gephi_viz.js
@@ -2,6 +2,14 @@ class GephiViz {
     constructor(objectId) {
         this.svgElement = this.getSVGObject(objectId)
         this.viewBoxOrig = this.svgElement.viewBox.baseVal
+        this.minViewX = this.viewBoxOrig.x
+        this.maxViewX = this.viewBoxOrig.x + this.viewBoxOrig.width
+        this.minViewY = this.viewBoxOrig.y
+        this.maxViewY = this.viewBoxOrig.y + this.viewBoxOrig.height
+        this.svgElement.style.userSelect = "none"
+        this.isClicked = false
+        this.zoomLevel = 1
+        this.zoomScale = 1.5
         this.initElements()
         this.cleanupClasses()
         this.addListeners()
@@ -77,24 +85,34 @@ class GephiViz {
     }
 
     zoomIn() {
+        this.zoomLevel += 1
         var viewBox = this.svgElement.viewBox.baseVal
-        viewBox.x = viewBox.x + viewBox.width / 4;
-        viewBox.y = viewBox.y + viewBox.height / 4;
-        viewBox.width = viewBox.width / 2;
-        viewBox.height = viewBox.height / 2;
+        viewBox.x = viewBox.x + viewBox.width * (this.zoomScale - 1) / (2 * this.zoomScale);
+        viewBox.y = viewBox.y + viewBox.height * (this.zoomScale - 1) / (2 * this.zoomScale);
+        viewBox.width = viewBox.width / this.zoomScale;
+        viewBox.height = viewBox.height / this.zoomScale;
     }
     zoomOut() {
-        var viewBox = this.svgElement.viewBox.baseVal
-        viewBox.x = viewBox.x - viewBox.width / 2;
-        viewBox.y = viewBox.y - viewBox.height / 2;
-        viewBox.width = viewBox.width * 2;
-        viewBox.height = viewBox.height * 2;
+        if (this.zoomLevel == 1) {
+            return
+        }
+        this.zoomLevel -= 1
+        let viewBox = this.svgElement.viewBox.baseVal
+        let newX = viewBox.x - viewBox.width * (this.zoomScale - 1) / 2;
+        let newY = viewBox.y - viewBox.height * (this.zoomScale - 1) / 2;
+        viewBox.width = viewBox.width * this.zoomScale;
+        viewBox.height = viewBox.height * this.zoomScale;
+        viewBox.x = Math.max(this.minViewX, Math.min(this.maxViewX-viewBox.width, newX))
+        viewBox.y = Math.max(this.minViewY, Math.min(this.maxViewY-viewBox.height, newY))
     }
 
     addListeners() {
         const classObj = this
         this.nodes.map(node => {
             node.addEventListener("mouseenter", event => {
+                if (this.isClicked) {
+                    return
+                }
                 this.applyElements(element => {
                     this.dimElement(element)
                 })
@@ -116,6 +134,36 @@ class GephiViz {
             }
             else {
                 this.zoomIn()
+            }
+        })
+
+        this.addMouseListener()
+    }
+
+    addMouseListener() {
+        this.isClicked = false
+        let x = 0
+        let y = 0
+        // let lastMove = Date.now()
+        this.svgElement.addEventListener("mousedown", event => {
+            x = event.offsetX
+            y = event.offsetY
+            this.isClicked = true
+        })
+        this.svgElement.addEventListener("mouseup", event => {
+            x = event.offsetX
+            y = event.offsetY
+            this.isClicked = false
+        })
+        this.svgElement.addEventListener("mousemove", event => {
+            if (this.isClicked) {
+                let viewBox = this.svgElement.viewBox.baseVal
+                let newX = viewBox.x + (x - event.offsetX)
+                let newY = viewBox.y + (y - event.offsetY)
+                viewBox.x = Math.max(this.minViewX, Math.min(this.maxViewX-viewBox.width, newX))
+                viewBox.y = Math.max(this.minViewY, Math.min(this.maxViewY-viewBox.height, newY))
+                x = event.offsetX
+                y = event.offsetY
             }
         })
     }
